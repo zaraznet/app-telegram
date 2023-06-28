066adf762e2fa20675bea7dc65e3be74eb7e08e0
kirill <1@com>
2023-06-28 23:20:52 +0300

    m

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from "react";
+
+import React, { useContext } from "react";
+import { CartContext } from "../Cart/CartContext";
 import "./Card.css";
 import Button from "../Button/Button";
-function Card({ food, onAdd, onRemove }) {
-  const [count, setCount] = useState(0);
-  const { title, Image, price, id } = food;
 
-  const handleIncrement = () => {
-    setCount(count + 1);
-    onAdd(food);
+
+function Card({ item }) {
+  const { addItemToCart, removeItemFromCart, getItemQuantity } = useContext(CartContext);
+  const { title, Image, price, id } = item;
+
+  const handleAddItem = () => {
+    addItemToCart(item);
   };
-  const handleDecrement = () => {
-    setCount(count - 1);
-    onRemove(food);
+
+  const handleRemoveItem = () => {
+    removeItemFromCart(item);
   };
+
+  const quantity = getItemQuantity(id);
 
   return (
     <div className="card">
-      <span
-        className={`${count !== 0 ? "card__badge" : "card__badge--hidden"}`}
-      >
-        {count}
+      <span className={`${quantity !== 0 ? "card__badge" : "card__badge--hidden"}`}>
+        {quantity}
       </span>
-      <div className="image__container">
-        <img src={Image} alt={title} />
+      <div >
+        <img className="image__container" src={Image}  alt="" /> 
       </div>
       <h4 className="card__title">
-        {title} . <span className="card__price">$ {price}</span>
+        {title} <span className="card__price"> {price} RUB</span>
       </h4>
-
       <div className="btn-container">
-        <Button title={"+"} type={"add"} onClick={handleIncrement} />
-        {count !== 0 ? (
-          <Button title={"-"} type={"remove"} onClick={handleDecrement} />
+        <Button title={"+"} type={"add"} onClick={handleAddItem} />
+        {quantity !== 0 ? (
+          <Button title={"-"} type={"remove"} onClick={handleRemoveItem} />
         ) : (
           ""
         )}
@@ -42,41 +44,4 @@ function Card({ food, onAdd, onRemove }) {
 
 export default Card;
 
-/*
-Этот код определяет функциональный React-компонент `Card`, который отображает карточку продукта с кнопками добавления и удаления. Компонент принимает 
- три свойства через пропсы: `food`, `onAdd` и `onRemove`.
 
-- `food` - объект, содержащий информацию о продукте, такую как заголовок, изображение, цена и идентификатор;
-- `onAdd` - функция, которая вызывается, когда пользователь добавляет продукт в корзину;
-- `onRemove` - функция, которая вызывается при удалении продукта из корзины.
-
-Компонент `Card` включает функцию-хук `useState`, которая используется для отслеживания количества выбранных продуктов. 
-Значение счетчика устанавливается равным `0` при создании компонента - `const [count, setCount] = useState(0);`.
-У компонента есть две функции для увеличения и уменьшения значения счетчика - `handleIncrement()` и `handleDecrement()`, каждая из которых также вызывает
- соответствующую функцию переданную в компонент через пропсы, для добавления или удаления продукта.
-Функциональный компонент отображает разметку JSX, которая содержит информацию о продукте, кнопки добавления и удаления продукта, а также `span`,
- который показывает количество продуктов, добавленных в корзину.
-Таким образом, этот код создает компонент карточки продукта, который может использоваться в React-приложениях для отображения элементов меню и управления
- количеством продуктов, выбранных пользователем.
-
-
-  import React, { useState } from "react"; - импортирует библиотеки React и хука useState.
-2. import "./Card.css"; - импортирует стилевой файл Card.css.
-3. import Button from "../Button/Button"; - импортирует компонент кнопки Button.
-4. function Card({ food, onAdd, onRemove }) { ... } - определяет функциональный компонент Card, который принимает три свойства через пропсы: food, onAdd, и onRemove.
-5. const [count, setCount] = useState(0); - использует хук useState, чтобы создать переменную count со значением 0 и функцию setCount. count используется
- для отслеживания количества добавленных продуктов.
-6. const { title, Image, price, id } = food; - деструктурирование объекта food, чтобы получить заголовок, изображение, цену и идентификатор продукта.
-7. const handleIncrement = () => { ... } - задает функцию handleIncrement, которая увеличивает количество продуктов на 1 и вызывает функцию onAdd с передачей
- в неё объекта food. Эта функция используется для добавления продукта в корзину.
-8. const handleDecrement = () => { ... } - задает функцию handleDecrement, которая уменьшает количество продуктов на 1 и вызывает функцию onRemove
- с передачей в неё объекта food. Эта функция используется для удаления продукта из корзины.
-9. <span ...>{count}</span> - отображает количество продуктов, добавленных в корзину, при условии, что этого количества больше 0.
-10. <img src={Image} alt={title} /> - отображает изображение продукта.
-11. <Button ... onClick={handleIncrement} /> - вызывает кнопку с +, используется для добавления продукта в корзину. При клике на кнопке вызывается функция
- handleIncrement.
-12. <Button ... onClick={handleDecrement} /> - вызывает кнопку с -, используется для удаления продукта из корзины. При клике на кнопке вызывается функция handleDecrement.
-13. export default Card; - экспорт по умолчанию функционального компонента Card.
-
-
-*/
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,43 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Cart.css";
 import Button from "../Button/Button";
-function Cart({ cartItems, onCheckout }) {
-  const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
+import { useCart } from "./CartContext";
 
-  return (
-    <div className="cart__container">
-      {cartItems.length === 0 ? "No items in cart" : ""}
-      <br /> <span className="">Total Price: ${totalPrice.toFixed(2)}</span>
-      <Button
-        title={`${cartItems.length === 0 ? "Order !" : "Checkout"} `}
-        type={"checkout"}
-        disable={cartItems.length === 0 ? true : false}
-        onClick={onCheckout}
-      />
-    </div>
-  );
+function Cart() { 
+  const { cartItems } = useCart();
+  
+  const navigate = useNavigate();
+ 
+  const handleCheckoutClick = () => {
+    navigate('/Order');
+  };
+  
+  const buttonTitle = "Корзина";
+ 
+  return (  
+    <>
+    <div className="cart__container">  
+        <Button  
+          title={buttonTitle}  
+          type={"checkout"}  
+          disable={cartItems.length === 0}  
+          onClick={handleCheckoutClick}
+        />   
+    </div>  
+    </>
+  ); 
 }
-
+ 
 export default Cart;
 
-/*
-Этот код представляет собой компонент корзины для приложения React. 
-Он принимает два свойства: cartItems, которое представляет собой массив товаров, добавленных в корзину, 
-и onCheckout, функцию, которая вызывается при нажатии на кнопку "Оформить заказ".
-В этом компоненте вычисляется общая стоимость товаров в корзине с помощью метода reduce(). 
-Если корзина пуста, выводится сообщение "No items in cart", в противном случае отображается общая стоимость товаров.
-Компонент также содержит кнопку "Оформить заказ". Если корзина пуста, кнопка становится неактивной (disabled). 
-При нажатии на кнопку вызывается функция onCheckout.
 
-1. import React from "react"; - импорт библиотеки React.
-2. import "./Cart.css"; - импорт стилевого файла Cart.css.
-3. import Button from "../Button/Button"; - импорт React-компонента Button, который располагается в Button.js.
-4. function Cart({ cartItems, onCheckout }) { - определение функционального компонента Cart. cartItems и onCheckout - это свойства, переданные компоненту через пропсы.
-5. const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0); - вычисляет общую стоимость товаров в корзине, используя метод reduce().
-6. <div className="cart__container"> - открывающийся тег <div> с установленным стилем CSS.
-7. {cartItems.length === 0 ? "No items in cart" : ""} - выводит сообщение, если корзина пуста.
-8. <br /> - разрыв строки.
-9. <span className="">Total Price: ${totalPrice.toFixed(2)}</span> - отображение общей стоимости с двумя знаками после запятой.
-10. <Button ... /> - вызывается компонент кнопки, используется для оформления заказа.
-11. export default Cart; - экспорт по умолчанию функционального компонента Cart.
-*/
